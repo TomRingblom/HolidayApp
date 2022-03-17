1df511d213f0ec58f3805d3730e1d27d3767b920
TomRingblom <t_ringblom@hotmail.com>
2022-03-17 14:14:43 +0100

    first commit

--- src/components/CountryInfo/CountryDays/CountryDays.js
+++ src/components/CountryInfo/CountryDays/CountryDays.js
@@ -1,7 +1,7 @@
 import "./CountryDays.css";
 
 export default function CountryDays(props) {
-    console.log(props.onDayInfo);
+    // console.log(props.onDayInfo);
   return (
     <div className="days">
       {props.onDayInfo.map((days) => (
