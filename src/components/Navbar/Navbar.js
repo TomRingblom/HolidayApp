d505f8ebd8e9cb1f2bebd810891da04291ee7f83
Michael S <Michael.Stojanovic@hotmail.com>
2022-03-22 17:00:38 +0100

    added navbar css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,20 +5,20 @@ import React from 'react'
 export default function Navbar(props) {
   return (
     <div className="navbar">
-      <ul>
-        <li>
+    
+      
           <a href="default.asp">Map</a>
-        </li>
-        <li>
+        
+      
           <a onClick={props.onSaveDays}>Saved Days</a>
-        </li>
-        <li>
+        
+      
           <a href="contact.asp"></a>
-        </li>
-        <li>
+        
+      
           <a href="about.asp"></a>
-        </li>
-      </ul>
+        
+      
     </div>
   );
 };
