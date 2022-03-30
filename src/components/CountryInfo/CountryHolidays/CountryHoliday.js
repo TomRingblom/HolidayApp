54ad121df1f00422c25133daacd6202ee4fa9020
Michael S <Michael.Stojanovic@hotmail.com>
2022-03-30 15:29:47 +0200

    added quick response on btn-click

--- src/components/CountryInfo/CountryHolidays/CountryHoliday.js
+++ src/components/CountryInfo/CountryHolidays/CountryHoliday.js
@@ -1,8 +1,9 @@
+import { useEffect, useState } from "react";
 import "./CountryHoliday.css"
 
 
 const CountryHoliday = (props) => {
-
+  
   const saveDay = () => {
     const key = Math.random().toString();
     const infoObject = {
@@ -10,14 +11,17 @@ const CountryHoliday = (props) => {
       date: props.date
     }
     localStorage.setItem(key, JSON.stringify(infoObject));
+    
   }
+
+  
 
   return (
     <div className="item">
       <h1 className="header">{props.name}</h1>
       <div>
         <span className="info">{props.date}</span>
-        <button className="btn-add-holiday" onClick={saveDay}>➕</button>
+        <button className="btn-add-holiday" onClick={saveDay}>➕</button> <span className="hidden-checkbox">✅ Day added.</span>
       </div>
     </div>
   );
